89d80d1f3b017c2a89458dd4baf7f6aa3f253381
Recleph M <lilrayzer1@gmail.com>
2021-09-24 03:02:37 -0700

    Fix issue where slider decimals give inaccurate results and Sync slider with segment controller

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -20,12 +20,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Set keypad to use only numpad with decimals
+        billAmountTextField.keyboardType = UIKeyboardType.decimalPad
+        billAmountTextField.keyboardAppearance = UIKeyboardAppearance.default
+        billAmountTextField.becomeFirstResponder()
+        
+        // Set default percentages to match active selection on view appear
+        tipPercentage.text = "15%"
+        percentageSlider.value = 15
     }
 
     @IBAction func modifyTip(_ sender: Any) {
+        // Sync the segment controller to active with the slider values
+        switch round(percentageSlider.value) {
+            case 15:
+                tipControl.selectedSegmentIndex = 0
+            case 18:
+                tipControl.selectedSegmentIndex = 1
+            case 20:
+                tipControl.selectedSegmentIndex = 2
+            default:
+                tipControl.selectedSegmentIndex = UISegmentedControl.noSegment
+        }
         let bill = Double(billAmountTextField.text!) ?? 0
         // Get Total tip by multiplying tip * tipPercentage
-        let percentage = Double(percentageSlider.value / 100)
+        // Round values to get a more consolidated result for user
+        let percentage = Double(round(percentageSlider.value) / 100)
         
         tipPercentage.text = String(format: "%.0f%%", percentageSlider.value)
         let tip = bill * percentage
@@ -42,6 +66,8 @@ class ViewController: UIViewController {
         let bill = Double(billAmountTextField.text!) ?? 0
         // Get Total tip by multiplying tip * tipPercentage
         let tipPercentageList = [0.15, 0.18, 0.20]
+        
+        // Sync the slider with the value of the segment controller
         switch tipControl.selectedSegmentIndex {
             case 0:
                 tipPercentage.text = "15%"
@@ -63,9 +89,6 @@ class ViewController: UIViewController {
         tipAmountLabel.text = String(format: "$%.2f", tip)
         // Update Total Amount
         totalLabel.text = String(format: "$%.2f", total)
-        
-        
     }
-    
 }
 
